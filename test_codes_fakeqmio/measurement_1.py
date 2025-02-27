cd5de12bb0591bd482860f26c079d88048fc0cad
carolFR13 <carolframa@gmail.com>
2025-02-27 20:36:57 +0100

    New (wrong) implementation of the algorithm + Using attributes not implemented for the FakeQmio backend

--- test_codes_fakeqmio/measurement_1.py
+++ test_codes_fakeqmio/measurement_1.py
@@ -7,50 +7,40 @@ num_qubits = 5
 
 backend = FakeQmio(gate_error = True, readout_error = True)
 
-# list to save results
-results_list = []
-
 # we initialice the circuit with *num_qubits* qubits and the 
 # same number of classical ones to measure them
 qc = QuantumCircuit(num_qubits, num_qubits)
 
+# ---- circuit definition -----
 qc.x(range(num_qubits)) # applying x gate: |0> -> |1>
 qc.delay(1, range(num_qubits), unit='us')  # delay of  1 µs
-qc.measure(range(num_qubits), range(num_qubits))
+qc.measure(range(num_qubits), range(num_qubits)) # this operation takes about 5 µs
 
 qct = transpile(qc, backend)
 
-num_repetitions = 100000
+#------ circuit execution ------
+n_repetitions = 1000 ; repetition_period = 0.001 #in s
 
 start_time = time.time() #initial time t = 0
 
-with open("results1.txt", "w") as f:
-    f.write("Repetition\tTime(s)\tState\n")
+n_shots = n_repetitions
+result = backend.run(qct, shots=n_repetitions, repetition_period = repetition_period, memory = True).result()  
+memory = result.get_memory(qct)
 
-# including try except to save each iteration in the file
-# in case the job is interrupted
+# absolute time the circuit lasted
+absolute_time = time.time() - start_time
 
-try: 
-    for i in range(num_repetitions):
-        job = backend.run(qct, shots=1)  
-        result = job.result()
-        counts = result.get_counts(qc)
+# free memory
+del result
 
-        # absolute time between measurements ?
-        absolute_time = time.time() - start_time
+#----- results ----
+print('The absolute exectution time was: ', absolute_time)
+print('The theoretical exectution time is: ', n_repetitions*repetition_period)
+print('memory:\n', memory)
 
-        for state in counts:
-            # saving the result in each iteration
-            with open("results1.txt", "a") as f:
-                f.write(f"{i + 1}\t{absolute_time:.6f}\t{state}\n")
-                f.flush()  # forcing the file to be immediately saved
 
-        print(f"Repetition {i+1}: Measured state = {state}, Absolute time = {absolute_time:.6f} s")
+with open("results1.txt", "a") as f:
+    f.write("Repetition\tState\n")
+    for (i,state) in enumerate(memory):
+        f.write(f"{i + 1}\t{state}\n")
 
-        # free memory
-        del job, result, counts
-
-except Exception:
-    print("\n Program interrupted.")
-
-print("Execution finalized. Results available in 'results1.txt'")
