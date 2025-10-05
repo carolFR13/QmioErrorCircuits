3f15e9fbbe933aca15e225c3d9aa4563cfee02d5
carolFR13 <carolframa@gmail.com>
2025-10-05 23:02:16 +0200

    Increasing qpu time in batch script

--- codes_qmio/job_qmio.sh
+++ codes_qmio/job_qmio.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks 1            # Total number of MPI tasks (if omitted, n=<node_total_cores>)
 #SBATCH --cpus-per-task 1     # Cores per task
 #SBATCH --mem-per-cpu 4G      # Memory per core
-#SBATCH --time 00:05:00       # Max run time (hh:mm:ss)
+#SBATCH --time 01:30:00       # Max run time (hh:mm:ss)
 
 module load gcc/12.3.0 qiskit/1.2.4-python-3.9.9 qiskit-experiments/0.7.0-python-3.9.9
 module load qmio-tools/0.1.3-python-3.9.9
